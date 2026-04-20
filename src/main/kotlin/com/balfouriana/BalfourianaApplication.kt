6e4fcdbe20145627e47f642c33874dff8a37d4ef
Alex Havryleshko <alexhavryleshko@gmail.com>
2026-04-20 11:11:31 +0100

    Spring Boot setup for Kotlin

---
+++ src/main/kotlin/com/balfouriana/BalfourianaApplication.kt
@@ -0,0 +1,11 @@
+package com.balfouriana
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BalfourianaApplication
+
+fun main(args: Array<String>) {
+	runApplication<BalfourianaApplication>(*args)
+}
---
+++ src/test/kotlin/com/balfouriana/BalfourianaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.balfouriana
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BalfourianaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
